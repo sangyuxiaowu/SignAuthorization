603ad898c2ec7b20486beeb56b42d0fb1dfb4463
桑榆肖物 <sang93@qq.com>
2024-04-26 23:38:32 +0800

    chore: 优化单元测试

--- TestSignAuthorization/UnitTest.cs
+++ TestSignAuthorization/UnitTest.cs
@@ -5,6 +5,12 @@ namespace TestSignAuthorization
     [TestClass]
     public class UnitTest
     {
+        private void AssertUrlStartsWith(string url, string expectedStart)
+        {
+            Console.WriteLine(url);
+            Assert.IsTrue(url.StartsWith(expectedStart));
+        }
+
         [TestMethod]
         public void MakeSign()
         {
@@ -18,66 +24,31 @@ namespace TestSignAuthorization
             Assert.IsTrue(sign.Length == 40);
         }
 
-        [TestMethod]
-        public void MakeSignUrl()
+        [DataTestMethod]
+        [DataRow("http://localhost:5177", "http://localhost:5177/?", null)]
+        [DataRow("/", "/?", null)]
+        [DataRow("/abc", "/abc?", null)]
+        [DataRow("/?ext=1", "/?ext=1&", "ext")]
+        [DataRow("/abc?ext=1", "/abc?ext=1&", "ext")]
+        [DataRow("abc/?ext=1", "abc/?ext=1", "ext")]
+        public void MakeSignUrl(string inputUrl, string expectedStart, string extra)
         {
-            var url = MakeSignAuthorization.MakeSignUrl("http://localhost:5177", new SignAuthorizationOptions());
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("http://localhost:5177/?"));
+            var url = MakeSignAuthorization.MakeSignUrl(inputUrl, new SignAuthorizationOptions() { nExtra = extra });
+            AssertUrlStartsWith(url, expectedStart);
         }
 
         [TestMethod]
-        public void MakeSignUrl_Path1()
-        {
-            var url = MakeSignAuthorization.MakeSignUrl("/", new SignAuthorizationOptions());
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("/?"));
-        }
-
-        [TestMethod]
-        public void MakeSignUrl_Path2()
-        {
-            var url = MakeSignAuthorization.MakeSignUrl("/abc", new SignAuthorizationOptions());
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("/abc?"));
-        }
-
-        [TestMethod]
-        public void MakeSignUrl_Path3()
-        {
-            var url = MakeSignAuthorization.MakeSignUrl("/?ext=1", new SignAuthorizationOptions() { nExtra = "ext" });
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("/?ext=1&"));
-        }
-
-        [TestMethod]
-        public void MakeSignUrl_Path4()
-        {
-            var url = MakeSignAuthorization.MakeSignUrl("/abc?ext=1", new SignAuthorizationOptions() { nExtra = "ext" });
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("/abc?ext=1&"));
-        }
-
-        [TestMethod]
-        public void MakeSignUrl_Path5()
+        public void MakeSignUrl_InvalidUrl()
         {
             try
             {
                 var url = MakeSignAuthorization.MakeSignUrl("/?#ext=1", new SignAuthorizationOptions() { nExtra = "ext" });
-                Assert.IsTrue(false);
+                Assert.Fail("Expected an exception to be thrown");
             }
             catch
             {
                 Assert.IsTrue(true);
             }
-        }
-
-        [TestMethod]
-        public void MakeSignUrl_Path6()
-        {
-            var url = MakeSignAuthorization.MakeSignUrl("abc/?ext=1", new SignAuthorizationOptions() { nExtra = "ext" });
-            Console.WriteLine(url);
-            Assert.IsTrue(url.StartsWith("abc/?ext=1"));
         }
     }
 }
